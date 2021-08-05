c91f6d27a6f6ff9c9d1d36e72fb21cbf812e9bc9
Ken Mawhinney <>
2021-08-05 16:44:37 -0400

    Add zeroconf client to look up the MQTT broker

---
+++ music-comms/src/discovery/__init__.py
@@ -0,0 +1,41 @@
+from zeroconf import ServiceInfo, Zeroconf
+
+"""
+Use this to look up a service when it is not expected to change or be updated.
+Usually this would be invoked in a loop polling until there is a response. If
+the application needs to react to changes in the service, perhaps it moves
+to a different host, then use the listener approach that is in find_services.py
+"""
+
+
+class FindServices():
+    zc: Zeroconf
+
+    def __init__(self):
+        self.zc = Zeroconf()
+
+    def get_service_info(self, type: str, name: str) -> ServiceInfo:
+        """
+        Retrieves the service info matching the given type and name. The name is
+        the unqualified service name, e.g. "DYLAN MQTT Server". Makes a query for the
+        service and waits a timeout (default 3 seconds) for a response. Returns none
+        if the query timesout.
+        :param type: The type of the service to be retrieved
+        :param name: The name of the service
+        :return: The matching service or none if there if the query does not respond
+        in less than 3 seconds.
+        """
+        return self.zc.get_service_info(type_=type, name=f"{name}.{type}")
+
+
+class ServiceInfoParser():
+    # This is the service info
+    info: ServiceInfo
+
+    def __init__(self, info: ServiceInfo):
+        if info is None:
+            raise ValueError("Info parameter must not be None")
+        self.info = info
+
+    def get_host_port(self) -> str:
+        return f"{self.info.server}:{self.info.port}"
---
+++ music-comms/src/discovery/find_service.py
@@ -0,0 +1,43 @@
+"""
+Uses Python-zeroconf to find services on the local network.
+"""
+from time import sleep
+
+from zeroconf import ServiceBrowser, Zeroconf, ServiceListener, ServiceInfo
+
+from discovery import FindServices, ServiceInfoParser
+
+
+class ZCListener(ServiceListener):
+
+    def remove_service(self, zeroconf, type, name):
+        print("Removed service:", zeroconf.get_service_info(type, name))
+
+    def add_service(self, zeroconf, type, name):
+        info: ServiceInfo = zeroconf.get_service_info(type, name)
+        print("Added service:", "type:", type, "name:", name, "info", info, info.server, info.port)
+
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        info: ServiceInfo = zeroconf.get_service_info(type, name)
+        print("Updated service:", "type:", type, "name:", name, "info", info, info.server, info.port)
+
+
+zeroconf = Zeroconf()
+listener = ZCListener()
+browser = ServiceBrowser(zeroconf, "_mqtt._tcp.local.", listener)
+try:
+    service_finder = FindServices()
+    type = "_mqtt._tcp.local."
+    name = "DYLAN MQTT Server"
+    while True:
+        info = service_finder.get_service_info(type, name)
+        if info is not None:
+            break
+        else:
+            print(f"Service {type} {name} not found, try again in a few seconds")
+            sleep(5)
+    parser = ServiceInfoParser(info)
+    print("info", parser.get_host_port())
+    input("Press enter to exit...\n\n")
+finally:
+    zeroconf.close()
